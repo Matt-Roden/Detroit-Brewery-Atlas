9af7d2dc261e2adea06ecbfdaec5323c41da909b
Matt Roden <mattroden92@gmail.com>
2021-09-23 14:45:13 -0600

    Successfully filter out Minnesota brewery and set cleaned data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import {cleanData} from '../utlis'
+import {cleanData} from '../../utilities'
 import './App.css';
 
 const App = () => {
@@ -10,7 +10,8 @@ const App = () => {
     try {
       const response = await fetch('https://api.openbrewerydb.org/breweries?by_city=detroit')
       const breweriesData = await response.json()
-      setAllBreweries(breweriesData)
+      const cleanedData = cleanData(breweriesData)
+      setAllBreweries(cleanedData)
     } catch (err) {
       console.log('Error: ', err)
     }
