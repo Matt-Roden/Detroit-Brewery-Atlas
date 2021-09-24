30b9de71ac513540a5cc0295d922f190497ff897
Matt Roden <mattroden92@gmail.com>
2021-09-23 21:47:51 -0600

    Write mapping function for building brewery cards

--- src/components/AllBreweries/AllBreweries.js
+++ src/components/AllBreweries/AllBreweries.js
@@ -5,12 +5,24 @@ const AllBreweries = ({ allBreweriesData }) => {
   const breweryCards = allBreweriesData.map((brewery) => {
     return (
       <BreweryCard
-        
+        key={brewery.id}
+        id={brewery.id}
+        name={brewery.name}
+        type={brewery.brewery_type}
+        street={brewery.street}
+        city={brewery.city}
+        state={brewery.state}
+        zip={brewery.postal_code}
+        phone={brewery.phone}
+        website={brewery.website_url}
+        imageSrc={brewery.image}
       />
     )
   })
 
   return (
-
+    <section className='all-breweries-container'>
+      {breweryCards}
+    </section>
   )
 }
