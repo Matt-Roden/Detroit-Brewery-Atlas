55e2eb2fb88b863e59502ba8d64789a42b710cb1
Matt Roden <mattroden92@gmail.com>
2021-09-23 22:03:47 -0600

    Finish initial build of card

--- src/components/BreweryCard/BreweryCard.js
+++ src/components/BreweryCard/BreweryCard.js
@@ -10,6 +10,13 @@ const BreweryCard = ({ name, type, street, city, state, zip, phone, website, ima
       <div className='info-container'>
         <h2>{name}</h2>
         <h3>{`Brewery Type: ${type}`}</h3>
+        <div className='address-container'>
+          <p className='street'>{street}</p>
+          <p className='city-state'>{`${city}, ${state}`}</p>
+          <p className='zipcode'>{zip}</p>
+        </div>
+        <h3 className='phone-number'>{phone}</h3>
+        <h3 className='website'>{website}</h3>
       </div>
     </article>
   )
