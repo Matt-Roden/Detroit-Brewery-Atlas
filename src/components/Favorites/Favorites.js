f761e2d052e9a61372e7016d4277c9af1fedd7e7
Matt Roden <mattroden92@gmail.com>
2021-09-24 19:16:03 -0600

    Build initial component structure and export

---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,11 @@
+import './Favorites.css'
+import BreweryCard from '../BreweryCard/BreweryCard'
+
+const Favorites = ({ favoritesList }) => {
+
+  return(
+    <>Hellow World</>
+  )
+}
+
+export default Favorites
