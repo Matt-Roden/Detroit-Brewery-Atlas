9ff428e601ed389f48c6ce7d3c3be496d3f5c457
Matt Roden <mattroden92@gmail.com>
2021-09-25 12:10:34 -0600

    Add button and hover styles

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom'
+import './LandingPage.css'
+// <Link to='/home'><button>enter</button></Link>
 
 const LandingPage = () => {
   return (
     <section className='landing-page-container'>
-      <h1>Welcome to, Detroit - Now let's drink</h1>
-      <Link to='/home'><button>enter</button></Link>
+      <h1 className='welcome-message'>Welcome to, Detroit - Now let's drink</h1>
+
+      <div className="btn">
+        <span>enter</span>
+      </div>
+
     </section>
   )
 }
