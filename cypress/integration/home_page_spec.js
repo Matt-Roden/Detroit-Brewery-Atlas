ea58e1c4dae25dbd801555e8f865b1049eca9a1b
Matt Roden <mattroden92@gmail.com>
2021-09-25 17:05:29 -0600

    Successfully stub 2 cards - begin writing it blocks

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -8,4 +8,24 @@ describe('Home page features and functionality', () => {
   it('Should confirm true to be true', () => {
     expect(true).to.equal(true)
   });
+
+  it('Should render a navbar with the corresponding elements', () => {
+
+  })
+
+  it('Should render two cards to the screen', () => {
+
+  })
+
+  it('Should show the correct information on the cards', () => {
+
+  })
+
+  it('Should allow the user to favorite breweries and change the favorite-icon', () => {
+
+  })
+
+  it('Should allow a user to unfavorite a brewery and change the favorite-icon back to original', () => {
+    
+  })
 }); // end describe block
