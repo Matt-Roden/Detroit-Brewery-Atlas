bef76817771a3c2f3ec4334a4cc086b7e57b2552
Matt Roden <mattroden92@gmail.com>
2021-09-25 11:41:30 -0600

    Add Links to header elements

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,23 @@
 import './Header.css'
 import logo from '../../images/d_logo.png'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <nav className='nav-bar'>
       <ul className='link-list'>
-        <li className='list-item-link'>Favorites</li>
-        <li className='list-item-link'>See All</li>
+        <li className='list-item-link'>
+          <Link
+            to={'/favorites'}>Favorites</Link>
+        </li>
+        <li className='list-item-link'>
+          <Link to={'/'}>See All</Link>
+        </li>
         <li className='list-item-link'>About</li>
       </ul>
-      <img className='d-logo' src={logo} alt='old english D'/>
+      <Link to={'/'}>
+        <img className='d-logo' src={logo} alt='old english D'/>
+      </Link>
     </nav>
   )
 }
