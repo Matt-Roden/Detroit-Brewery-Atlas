fe373262d4dd21eff492e173ae12d38eb093a497
Matt Roden <mattroden92@gmail.com>
2021-09-26 13:21:46 -0600

    Write and pass all landing page tests

---
+++ cypress/integration/landing_page_spec.js
@@ -0,0 +1,21 @@
+beforeEach(() => {
+  cy.intercept('GET', 'https://api.openbrewerydb.org/breweries?by_city=detroit', { fixture: 'breweries' }).as('breweries')
+  cy.visit('http://localhost:3000/')
+})
+
+describe('Landing page features and functionality', () => {
+
+  it('Should be on the correct url', () => {
+    cy.url().should('include', '/')
+  });
+
+  it('Should render a welcome message and a button', () => {
+    cy.get('h1.welcome-message').contains('Welcome to Detroit - Now let\'s drink')
+    cy.get('div.btn').find('span.btn-text').contains('enter')
+  });
+
+  it('Should be take the user to the home page when the enter button is clicked', () => {
+    cy.get('div.btn').click()
+    cy.url().should('include', '/home')
+  });
+}); //end describe block
